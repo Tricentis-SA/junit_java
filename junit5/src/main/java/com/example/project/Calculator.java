1aec35ad2d8643daef51c923fa34cbe64b05713c
jarrod-grayson <52757778+jarrod-grayson@users.noreply.github.com>
2021-07-08 15:46:22 -0400

    Update Calculator.java

--- junit5/src/main/java/com/example/project/Calculator.java
+++ junit5/src/main/java/com/example/project/Calculator.java
@@ -19,5 +19,20 @@ public class Calculator {
 	public int subtract (int a, int b) {
 		return a-b;
 	}
+	
+	public long multiply (int a, int b) {
+		return a * b;
+	}
+	
+	public double divide(int a, int b) {
+		double result;
+		if (b == 0) {
+		throw new IllegalArgumentException("Divisor cannot divide by zero");
+		} else {
+		result = Double.valueOf(a)/Double.valueOf(b);
+		}
+		return result;
+			}
+	}
 
 }
